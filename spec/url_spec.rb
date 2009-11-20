b7a82e97941673f23411c39eb8d1651d8e408847
John F. Miller <emperor@antarestrader.com>
2009-11-19 20:41:15 -0800

    encoding

--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -1,21 +1,30 @@
 require File.join( File.dirname(__FILE__),  "spec_helper" )
 
-describe "shrinking url" do
+describe "Hi" do
   include Rack::Test::Methods
   
   def app
     Sinatra::Application
   end
+  
+  describe "shrinking url" do
 
-  it %q{shoud return "I'm Tiny" if there no query string} do
-    get '/'
-    last_response.body.should == "I'm Tiny"
+    it %q{shoud return "I'm Tiny" if there no query string} do
+      get '/'
+      last_response.body.should == "I'm Tiny"
+    end
+    
+    it %q{shoud NOT return "I'm Tiny" if there is a query string} do
+      get '/', :t=> Rack::Utils.escape('http://www.webvanta.com/')
+      last_response.body.should_not == "I'm Tiny"
+    end
+    
+    
   end
   
-  it %q{shoud NOT return "I'm Tiny" if there is a query string} do
-    get '/', :t=> Rack::Utils.escape('http://www.webvanta.com/')
-    last_response.body.should_not == "I'm Tiny"
+  describe "expanding urls"
+    before :each do
+      get '/', :t=> Rack::Utils.escape("http://www.webvanta.com/#{rand}")
+      @key = 
+    end 
   end
-  
-  
-end
